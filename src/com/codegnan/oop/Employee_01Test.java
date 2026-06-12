e311b7cf2ddeff2033050528531b18211dc47acc
JayaSri1910 <gandaboinajayasri@gmail.com>
2026-06-12 15:22:42 +0530

    feat: add Employee_01Test class

---
+++ src/com/codegnan/oop/Employee_01Test.java
@@ -0,0 +1,13 @@
+package com.codegnan.oop;
+
+public class Employee_01Test {
+	public static void main(String[] args) {
+		Employee_01 emp=new Employee_01(111,"mahi",30000);
+		emp.displayDetails();
+		emp.setId(101);
+		emp.setName("malli");
+		emp.setSalary(50000);
+		emp.displayDetails();
+}
+}
+	
